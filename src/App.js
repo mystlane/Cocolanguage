04dfa7aa497bac6cc1424ddc65090106235a3ac8
@mystlane <lalaineperez413@gmail.com>
2024-03-15 14:53:06 +0800

    React Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; // Import React
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'), // Serve static files from the 'dist' directory
+        compress: true,
+        port: 3000,
+        open: true,
+    },
+};
